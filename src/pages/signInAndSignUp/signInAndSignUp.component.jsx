31050f69bcd43082130ba97b74fcecdf36eddd50
Diego Corrales <diegoamcorrales@gmail.com>
2019-12-12 17:51:15 -0800

    implemented firebase utils, uncluded ability to store authentidated users

--- src/pages/signInAndSignUp/signInAndSignUp.component.jsx
+++ src/pages/signInAndSignUp/signInAndSignUp.component.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 
 import SignIn from "../../components/signIn/signIn.component";
+import SignUp from "../../components/signUp/signUp.component";
 
 import "./signInAndSignUp.styles.scss";
 
 const SignInAndSignUp = () => (
   <div className="sign-in-and-sign-up">
     <SignIn />
+    <SignUp />
   </div>
 );
 
